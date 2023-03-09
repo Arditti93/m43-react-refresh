648fb24321585c6a7e526481eca7a5b570497b34
Arditti93 <alex.arditti@hotmail.com>
2023-03-09 10:57:47 +0000

    removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-// import MovieCard from './componets/MovieCard'
+
 import CreateUser from './componets/CreateUser'
 import ReadUsers from './componets/ReadUsers'
 import Login from './componets/Login'
 import UpdateUser from './componets/UpdateUser'
 import DeleteUser from './componets/DeleteUser'
-import MovieCard from './componets/MovieCard'
+
 
 import { getCookie } from './common'
 import { authCheck } from './utils'
 
-
-const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=7d2e5ef7"
 
 const App = () => {
 
